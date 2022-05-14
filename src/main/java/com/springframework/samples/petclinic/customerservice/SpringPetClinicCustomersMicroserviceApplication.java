a3bd254395bb04055564cfec40d9042fb5fda281
seshasai-VVIT539 <seshasai8121@gmail.com>
2022-05-14 08:46:32 +0530

    init commit

---
+++ src/main/java/com/springframework/samples/petclinic/customerservice/SpringPetClinicCustomersMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.samples.petclinic.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicCustomersMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicCustomersMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/samples/petclinic/customerservice/SpringPetClinicCustomersMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.samples.petclinic.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicCustomersMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
